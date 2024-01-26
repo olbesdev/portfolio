cbc8c0432af535a315db3ecdba9873ff6b0e2bab
Jonathan Olbes <olbesdev@gmail.com>
2024-01-26 18:30:06 -0500

    Define DefaultValue on textarea

--- components/Contact.jsx
+++ components/Contact.jsx
@@ -17,7 +17,7 @@ const Contact = () => {
             <label htmlFor="" className='text-slate-50 font-semibold'>Email</label>
             <input type="email" className='block w-full border-2 border-slate-200 rounded-md p-2 mt-1' />
             <label htmlFor="" className='text-slate-50 font-semibold'>Message</label>
-            <textarea name="" id="" cols="30" rows="10" className='block w-full border-2 border-slate-200 rounded-md p-2 mt-1'>Write a message...</textarea>
+            <textarea name="" id="" cols="30" rows="10" defaultValue="Write text..." className='block w-full border-2 border-slate-200 rounded-md p-2 mt-1'></textarea>
             <button className='bg-slate-400 hover:bg-teal-500 text-white mt-4 font-semibold px-5 py-2 flex items-center gap-2 text-[1rem] rounded-full focus:scale hover:scale-105 transition-all'>Sumbit</button>
           </form>
         </div>
