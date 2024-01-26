08fa7f26fe1cf27f8592b15dd413442545564cae
Jonathan Olbes <olbesdev@gmail.com>
2024-01-25 21:26:28 -0500

    Add SectionHeading component

---
+++ components/SectionHeading.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+const SectionHeading = ({children}) => {
+  return (
+    <h2 className='text-3xl text-slate-100 font-medium capitalize mb-10 text-center'>{children}</h2>
+  )
+}
+
+export default SectionHeading
